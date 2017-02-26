f30c6af613ba6b53b99754729218bcb31de3322c
planbmanjb <planbmanjb@gmail.com>
2017-02-26 01:21:28 -0500

    first commit

---
+++ wiseperson.js
@@ -0,0 +1,7 @@
+function wisePerson(wiseType, whatToSay) {
+  var Type = "A wise " + wiseType + " once said ";
+  var ToSay = whatToSay + "!";
+
+  return Type + ToSay;
+}
+wisePerson('man','Compound interest is greatest invention of the 20th century');
