0fea448ad15e29b354d53508ac8862463ab34363
planbmanjb <planbmanjb@gmail.com>
2017-03-01 00:06:00 -0500

    version 5

---
+++ isdivisible.js
@@ -0,0 +1,33 @@
+function isDivisible(divisee, divisor) {
+ var remainder =  divisee % divisor;
+  if (remainder === 0) {
+       return true;
+  }
+  else {
+       return false;
+  }
+}
+
+
+/* From here down, you are not expected to
+   understand.... for now :)
+
+
+   Nothing to see here!
+
+*/
+
+
+
+// tests
+
+function testIsDivisible() {
+  if (isDivisible(10, 2) && !isDivisible(11, 2)) {
+    console.log('SUCCESS: `isDivisible` is working');
+  }
+  else {
+    console.log('FAILURE: `isDivisible` is not working');
+  }
+}
+
+testIsDivisible();
---
+++ tempconvert.js
@@ -0,0 +1,48 @@
+function celsToFahr(celsTemp) {
+ newFahr = celsTemp * 1.8 + 32;
+  return newFahr;
+}
+
+function fahrToCels(fahrTemp) {
+  newCels = (fahrTemp - 32) / 1.8;
+  return newCels;
+}
+
+
+/* From here down, you are not expected to
+   understand.... for now :)
+
+
+   Nothing to see here!
+
+*/
+
+
+
+// tests
+
+function testConversion(fn, input, expected) {
+  if (fn(input) === expected) {
+    console.log('SUCCESS: `' + fn.name + '` is working');
+    return true;
+  }
+  else {
+    console.log('FAILURE: `' + fn.name + '` is not working');
+    return false;
+  }
+}
+
+function testConverters() {
+  var cel2FahrInput = 100;
+  var cel2FahrExpect = 212;
+  var fahr2CelInput = 32;
+  var fahr2CelExpect = 0;
+
+  if (testConversion(celsToFahr, cel2FahrInput, cel2FahrExpect) &&
+     testConversion(fahrToCels, fahr2CelInput, fahr2CelExpect)) {
+    console.log('SUCCESS: All tests passing');
+  }
+  else {
+    console.log('FAILURE: Some tests are failing');
+  }
+}
